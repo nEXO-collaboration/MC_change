a58334cbc28c16de827f4d4655e35cac9c8c47aa
alexis-s <alexis4@stanford.edu>
2017-03-07 15:24:05 -0800

    reorganized, somehow fixed seg fault when reading n_entries

--- run.py
+++ run.py
@@ -1,54 +1,51 @@
 #!/usr/bin/env python
 #-*-coding: utf-8 -*-
+from ROOT import TFile
 import os
 import sys
 import Sniper
 
 def get_entries(filename):
-  from ROOT import TFile
   tfile = TFile(filename)
   tree = tfile.Get("nEXOevents")
   n_entries = tree.GetEntries()
-  tfile.Close()
   return n_entries
 
-task = Sniper.Task("mytask")
-task.asTop()
-#task.setLogLevel(0)
+def process_file(filename):
+  task = Sniper.Task("mytask")
+  task.asTop()
+  #task.setLogLevel(0)
 
-# = arguments =
-filename =  sys.argv[1]
-inputmc = os.path.basename(filename)
-inputmcdir = os.path.dirname(filename)
-if inputmcdir == "": inputmcdir = "./"
+  # = arguments =
+  inputmc = os.path.basename(filename)
+  inputmcdir = os.path.dirname(filename)
+  if inputmcdir == "": inputmcdir = "./"
 
-# = rootio =
-import RootIOSvc
-ros = task.createSvc("RootOutputSvc/OutputSvc")
-ros.property("OutputStreams").set({"/Event/Sim": "changed_%s" % inputmc})
+  # = rootio =
+  import RootIOSvc
+  ros = task.createSvc("RootOutputSvc/OutputSvc")
+  ros.property("OutputStreams").set({"/Event/Sim": "changed_%s" % inputmc})
 
-# = BufferMemMgr =
-import BufferMemMgr
-bufMgr = task.createSvc("BufferMemMgr")
-bufMgr.property("TimeWindow").set([0, 0]);
+  # = BufferMemMgr =
+  import BufferMemMgr
+  bufMgr = task.createSvc("BufferMemMgr")
+  bufMgr.property("TimeWindow").set([0, 0]);
 
-# = geometry service =
-import Geometry
-simgeomsvc = task.createSvc("SimGeomSvc")
+  # = geometry service =
+  import Geometry
+  simgeomsvc = task.createSvc("SimGeomSvc")
 
-# = MC_change = 
-Sniper.loadDll("libMyAlg.so")
-myalg = task.createAlg("MC_change/myalg")
-myalg.property("InputMCName").set(inputmc)
-myalg.property("InputMCDir").set(inputmcdir)
+  # = MC_change = 
+  Sniper.loadDll("libMyAlg.so")
+  myalg = task.createAlg("MC_change/myalg")
+  myalg.property("InputMCName").set(inputmc)
+  myalg.property("InputMCDir").set(inputmcdir)
 
+  n_entries = get_entries(filename) 
+  task.setEvtMax(n_entries)
+  task.show()
+  task.run()
 
-n_entries = 48
-#n_entries = get_entries(filename) # causes seg fault
-print "n_entries", n_entries
-task.setEvtMax(n_entries)
-#task.setEvtMax(-1) doesn't work
-task.show()
-print "--> running"
-task.run()
+if __name__ == "__main__":
 
+  process_file(sys.argv[1])
